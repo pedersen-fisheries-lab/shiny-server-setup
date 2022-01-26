057c9191c9f8707b87e8147b7b5757a34024650c
Valentin <valentin.lucet@gmail.com>
2022-01-25 19:10:29 -0500

    finish setup

--- shiny_server_setup.sh
+++ shiny_server_setup.sh
@@ -4,6 +4,7 @@
 apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+apt update
 apt install r-base
 
 # Print version
@@ -18,3 +19,7 @@ su - \
 apt install gdebi-core
 wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.17.973-amd64.deb
 sudo gdebi shiny-server-1.5.17.973-amd64.deb
+
+# Start server
+systemctl status shiny-server.service
+cp -r test-app/ /srv/shiny-server/
